0882b6a719831892a107339edb2894581185631c
David Zhu <shaowenzhu@dal.ca>
2024-10-28 20:58:40 -0300

    feat: fetch boxes

---
+++ MemoryBox/Network/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  MemoryBox
+//
+//  Created by David Zhu on 2024-10-28.
+//
+
+import Foundation
---
+++ MemoryBox/View/Home/HomeView-ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView-ViewModel.swift
+//  MemoryBox
+//
+//  Created by David Zhu on 2024-10-28.
+//
+
+import Foundation
